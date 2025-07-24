bb96bf700f6993cb82c79dd0bb1667c5e2a9e629
Fcooperation <thefcooperation@gmail.com>
2025-07-24 14:05:14 +0100

    Update fcrawler.js

--- fcrawler.js
+++ fcrawler.js
@@ -1,6 +1,6 @@
 const { execSync } = require('child_process');
 const fs = require('fs');
-const url = process.argv[2] || 'https://example.com';
+const url = process.argv[2] || 'https://vm.tiktok.com/ZSSeQ7KNb/ This post is shared via TikTok Lite. Download TikTok Lite to enjoy more posts:  https://www.tiktok.com/tiktoklite';
 const outputDir = './output';
 
 if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
